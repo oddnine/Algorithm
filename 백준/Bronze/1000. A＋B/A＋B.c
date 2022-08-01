13256482aed3fc9efc8096a9b0d170a456f34209
Hyuk <90389323+gugakdaisuki@users.noreply.github.com>
2022-08-01 10:21:14 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    if(a>0 && b<10){
+        printf("%d", a+b);
+        
+        return 0;
+    }
+}
