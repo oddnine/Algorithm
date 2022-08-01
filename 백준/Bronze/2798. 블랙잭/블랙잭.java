672aa78f2bc0dfbab48558a6b616409593c07ef7
Hyuk <90389323+gugakdaisuki@users.noreply.github.com>
2022-08-01 10:30:07 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int max = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] > max) {
+                        max = arr[i] + arr[j] + arr[k];
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
